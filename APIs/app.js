20b7f5c4ea8b3cc306af49aa0cc60c561170c261
cdrishinski <cdrishinski@gmail.com>
2020-02-27 13:59:06 -0700

    more prac

--- APIs/app.js
+++ APIs/app.js
@@ -1,17 +1,26 @@
-const request = require('request');
+const request = require('request')
+var rp = require('request-promise');;
 const axios = require('axios');
 
 console.log('my api call get me...')
 // axios.get()
 
+axios({
+    method: 'get',
+    url: 'https://jsonplaceholder.typicode.com/users/1',
+    responseType: 'json'
+  })
+    .then(function (response) {
+      console.log(response.data.name)
+    });
 
-request('https://jsonplaceholder.typicode.com/users/', function (error, response, body) {
-   // eval(require('locus'))
-    if(!error && response.statusCode == 200) {
-        const parsedData = JSON.parse(body)
-        console.log(parsedData[0].name, parsedData[1].name, parsedData[2].name)
+// rp('https://jsonplaceholder.typicode.com/users/')
+//     .then((body)=>{
+//         const parsedData = JSON.parse(body)
+//         console.log(parsedData[0])
+//     })
+//     .catch((err) =>{
+//         console.log('Error!', err)
+//     })
 
-    }
-
-});
-
+ 
